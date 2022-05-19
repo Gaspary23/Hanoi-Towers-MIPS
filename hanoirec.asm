6318c9a1a3aeae3812d4fe3fad8ea79a05b1d424
Gaspary23 <pgaspary23@gmail.com>
2022-05-19 16:29:17 -0300

    update: enhanced comments

--- hanoirec.asm
+++ hanoirec.asm
@@ -1,6 +1,8 @@
 # Torres de Hanoi - OAP
 #
-# Authors: Pedro Gaspary (21101429), Bernardo Zomer (21103639), Lucas Cunhas (21101060)
+# Authors: Pedro da Cunha Gaspary (21101429), 
+#	   Bernardo Barzotto Zomer (21103639), 
+#          Lucas Marchesan Cunha (21101060)
 
 .data
 texto0: .asciiz "\nTorres de Hanoi"
@@ -17,7 +19,7 @@ texto3: .asciiz "\n\nDeseja testar de novo? (0-nao, outro numero-sim): "
 	syscall				#
 	
 MAIN:
-	move 	$v1, $zero	# Zera o contador de movimentos para nao influenciar multiplas execucoes
+	move 	$v1, $zero		# Zera o contador de movimentos para nao influenciar multiplas execucoes
 
 	# Texto1
 	li	$v0, 4			#
@@ -55,6 +57,7 @@ MAIN:
 	# Saida do programa
 	li	$v0, 10 	        # Fim do programa
    	syscall				#
+   	
 #############################################################################################
 # Algoritmo recursivo de Hanoi
 HANOI:
@@ -62,38 +65,36 @@ HANOI:
     	bne 	$a0, 1, ELSE		# Se tiver mais de um disco, pula para o else
     	jr 	$ra			# Se soh tiver um disco, retorna
 ELSE:
-    	# Aloca espaco e bota os pinos na pilha
+    	# Aloca espaco e bota os argumentos na pilha
     	addi 	$sp, $sp, -20		# Aloca 20 bytes para 5 words
-    	sw 	$ra, 16($sp)		# Guarda o endereco de retorno de $ra
+    	sw 	$ra, 16($sp)		# Guarda o endereco de retorno em $ra
     	sw 	$a2, 12($sp)		# Guarda o pino extra
     	sw 	$a3, 8($sp)		# Guarda o pino destino
     	sw 	$a1, 4($sp)		# Guarda o pino fonte
 	sw 	$a0, 0($sp)		# Guarda o numero de discos
 	    
    	# han_move_tower(disk - 1, source, spare, dest): 
-    	# Movimenta os discos e chama HANOI de novo
     	move 	$t3, $a2		#
-    	move 	$a2, $a3		# Troca o disco do extra com o do destino
+    	move 	$a2, $a3		# Troca o disco de extra para destino
     	move 	$a3, $t3	 	# 
     	addi 	$a0, $a0, -1		# Decrementa o numero de discos 		
     	jal 	HANOI   		# Chamada recursiva
     	
-      	# Pega o que esta na pilha
-    	lw 	$ra, 16($sp)		# Carrega o endereco de retorno
+      	# Recebe os argumentos da pilha
+    	lw 	$ra, 16($sp)		# Carrega o endereco de retorno para $ra
     	lw 	$a2, 12($sp)		# Carrega o pino extra 
     	lw 	$a3, 8($sp)		# Carrega o pino destino
     	lw 	$a1, 4($sp)		# Carrega o pino fonte
     	lw 	$a0, 0($sp)		# Carrega o numero de discos
     
     	# han_move_tower(disk - 1, spare, dest, source):
-    	# Movimenta os discos e chama HANOI de novo
     	move 	$t3, $a2		# 
-    	move 	$a2, $a1		# Troca o disco do extra com o do destino
+    	move 	$a2, $a1		# Troca o disco de extra para fonte
     	move 	$a1, $t3		# 
     	addi 	$a0, $a0, -1		# Decrementa o numero de discos  
     	jal 	HANOI			# Chamada recursiva
     	
     	# Sai do algoritmo
-    	lw 	$ra, 16($sp)		# Bota o endereco de retorno no $ra
+    	lw 	$ra, 16($sp)		# Bota o endereco de retorno em $ra
     	addi 	$sp, $sp, 20		# Limpa a pilha
     	jr 	$ra    			# Salta para o endereco de retorno em $ra
